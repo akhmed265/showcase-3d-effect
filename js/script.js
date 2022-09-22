4569148b38479e72776e19de47d46feab09dcde1
Ахмед <r'p1MU)%+E.5WJSz8uVqu0m>
2022-09-22 20:19:53 +0300

    fix code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  const showSlider = new  Swiper('showcase-carousel', {
+    loop: true,
+    slidesPerView: 3
+  })
+
   document.querySelector('video').playbackRate = 2
-
-
 }) 
