eeabc7c86f73321f4b8cadac399ac82071b42282
Ахмед <r'p1MU)%+E.5WJSz8uVqu0m>
2022-09-25 16:44:02 +0300

    finished

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-  const showSlider = new  Swiper('showcase-carousel', {
+  const showSlider = new Swiper('.showcase-carousel', {
     loop: true,
-    slidesPerView: 3
-  })
+    slidesPerView: 3,
+    speed: 1800,
+    centeredSlides: true,
+    navigation: {
+      nextEl: '.showcase-navigation__next',
+      prevEl: '.showcase-navigation__prev'
+    }
+  });
 
-  document.querySelector('video').playbackRate = 2
-}) 
+  document.querySelector('video').playbackRate = 2;
+});
